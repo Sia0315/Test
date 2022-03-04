3ded6ae6480a081966d55ef5c9951d6d8995e86e
Sia0315 <amu1618@gmail.com>
2022-03-04 10:23:57 +0800

    測試

---
+++ flaskBootstrap12/main.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template
+from tableViews.table import tableApp
+from tableViews.youbike2 import youbikeApp
+#from sqlViews.loto import sqlApp
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hosikawa-sara'
+app.register_blueprint(tableApp)
+app.register_blueprint(youbikeApp)
+app.register_blueprint(sqlApp)
+
+@app.errorhandler(404)
+def error404(err):
+    return render_template('error404.html'),404
+
+@app.route('/')
+def index():
+    #return '<div style="font-size:2rem;color:red"> Hello! World</div>'
+    return render_template('index.html')
+
+@app.route('/layout')
+@app.route('/layout/box')
+def layout():
+    return render_template('layout.html',name='layout')
+
+@app.route('/layout/container')
+def container():
+    return render_template('container.html',name="container")
+
+@app.route('/layout/columns')
+def columns():
+    return render_template('container.html',name="columns")
+#ダウンロード中 ダウンロード成功 セーブ成功
+@app.route('/price')
+def price():
+    return render_template('price.html')
